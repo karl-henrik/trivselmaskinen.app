7d07a1cc18fff666c1d5d6234154a2b9d768b574
Daniel Hindrikes <daniel@hindrikes.se>
2014-09-10 12:36:30 +0200

    created solution

---
+++ source/Trivselappen/Trivselappen.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Trivselappen.WinPhone.Resources;
+
+namespace Trivselappen.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
