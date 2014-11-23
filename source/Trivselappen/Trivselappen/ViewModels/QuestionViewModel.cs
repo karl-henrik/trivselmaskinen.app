e1af21d2b224300721065f79c6abdc6a5a1855c1
Satunorr <satunorr@gmail.com>
2014-11-23 20:55:15 +0100

    Added reply buttons among a few other things

--- source/Trivselappen/Trivselappen/ViewModels/QuestionViewModel.cs
+++ source/Trivselappen/Trivselappen/ViewModels/QuestionViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Trivselappen.Models;
 
 namespace Trivselappen.ViewModels
 {
@@ -14,18 +15,30 @@ namespace Trivselappen.ViewModels
 
         public QuestionViewModel()
         {
-            Question = "The Question";
+            Question q = new Question();
+            q.Text = "The question for today! Click one of the buttons to reply."; //ToDo: ska hämtas från backend.
+            q.QuestionId = 0; //ToDo: ska sättas till id som hämtas ihop med frågan från backend.
+            TodaysQuestion = q.Text;
+            TodaysQuestionId = q.QuestionId;
+
         }
 
         private string _question;
-        public string Question { get{
+        public string TodaysQuestion { get{
             return _question;
         } 
             set{
                 _question = value;
-                RaisePropertyChanged("Question");
+                RaisePropertyChanged(TodaysQuestion);
                 }
         }
+        private int _questionId;
+        private int TodaysQuestionId { get{
+            return _questionId;
+        }
+            set { _questionId = value; }
+        }
+
         string _greeting;
         public string Greeting
         {
@@ -33,20 +46,77 @@ namespace Trivselappen.ViewModels
             set { _greeting = value; RaisePropertyChanged("Greeting"); }
         }
 
-        public ICommand Register
+        //Kommentar till nedan ICommands: Borde man inte kunna lösa detta med switch istället? All kod är ju lika utom answer.Value..
+        public ICommand Register1
         {
             get
             {
                 return new Command(
                     () =>
                     {
-                        var text = Question;
-                        Greeting = String.Format("Your answer has been saved");
+                        Answer answer = new Answer();
+                        answer.Value = 1;
+                        answer.DeviceId = "ToDo";
+                        answer.Timestamp = DateTime.Now;
+                        answer.QuestionId = _questionId;
+                        Greeting = String.Format("Your answer has been saved!");
+                    });
+            }
+
+        }
+        public ICommand Register2
+        {
+            get
+            {
+                return new Command(
+                    () =>
+                    {
+                        Answer answer = new Answer();
+                        answer.Value = 2;
+                        answer.DeviceId = "ToDo";
+                        answer.Timestamp = DateTime.Now;
+                        answer.QuestionId = _questionId;
+                        Greeting = String.Format("Your answer has been saved!");
                     });
             }
 
         }
 
+        public ICommand Register3
+        {
+            get
+            {
+                return new Command(
+                    () =>
+                    {
+                        Answer answer = new Answer();
+                        answer.Value = 3;
+                        answer.DeviceId = "ToDo";
+                        answer.Timestamp = DateTime.Now;
+                        answer.QuestionId = _questionId;
+                        Greeting = String.Format("Your answer has been saved!");
+                    });
+            }
+
+        }
+
+        public ICommand Register4
+        {
+            get
+            {
+                return new Command(
+                    () =>
+                    {
+                        Answer answer = new Answer();
+                        answer.Value = 4;
+                        answer.DeviceId = "ToDo";
+                        answer.Timestamp = DateTime.Now;
+                        answer.QuestionId = _questionId;
+                        Greeting = String.Format("Your answer has been saved!");
+                    });
+            }
+
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
--- source/Trivselappen/Trivselappen/Views/QuestionView.xaml.cs
+++ source/Trivselappen/Trivselappen/Views/QuestionView.xaml.cs
@@ -14,6 +14,7 @@ namespace Trivselappen.Views
             InitializeComponent();
 
             BindingContext = new QuestionViewModel();
+            
         }
     }
 }
