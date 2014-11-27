e04b9bd2512636b5c0650827088cec6cf1402edf
Satunorr <satunorr@gmail.com>
2014-11-27 11:16:45 +0100

    Added logo

--- source/Trivselappen/Trivselappen.Android/Resources/Resource.Designer.cs
+++ source/Trivselappen/Trivselappen.Android/Resources/Resource.Designer.cs
@@ -50,6 +50,9 @@ namespace Trivselappen.Droid
 			// aapt resource value: 0x7f020001
 			public const int monoandroidsplash = 2130837505;
 			
+			// aapt resource value: 0x7f020002
+			public const int Sogeti = 2130837506;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
