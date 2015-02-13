a74703ad94d6f4f200c91f9d057cc4b30f6dc87f
Daniel Hindrikes <daniel@hindrikes.se>
2015-02-13 10:00:28 +0100

    interfaces

---
+++ source/Trivselappen/Trivselappen/ServiceAgents/IDeviceServiceAgent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trivselappen.Models;
+
+namespace Trivselappen.ServiceAgents
+{
+    public interface IDeviceServiceAgent
+    {
+        Task<bool> RegisterDeviceAsync(Device device);
+    }
+}
